53ce44b5a125e93d6ac63958b9f1bd7412f45011
Neal Jay Robison <nealjrobison@gmail.com>
2019-01-24 15:08:58 -0500

    implement Crypt function

--- lfsr.py
+++ lfsr.py
@@ -1,6 +1,14 @@
-class LSFR:
-    def _init_(self, origin):
-        self.origin = origin
+def Crypt(data, iv):
+    for i in data:
+        fback = 0x87654321
+        lsb = 1 & iv
+        if (lsb == 1):
+            iv = ((iv >> 1) ^ fback)
+        if lsb:
+            iv >>= 1
+        yield i ^ iv
 
-    def Crypt(self):
-        
+
+print(bytes(Crypt(b'apple', 0x78)))
+temp = bytes(Crypt(b'apple', 0x78))
+print(bytes(Crypt(temp, 0x78)))
